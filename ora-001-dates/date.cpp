febda1ad2a95de67a656541a6799414244e16380
Dr. Ádám Csapó <adam.csapo@maxwhere.com>
2022-09-09 12:08:06 +0200

    refactor: date osztaly kiszervezese - 3

--- ora-001-dates/date.cpp
+++ ora-001-dates/date.cpp
@@ -20,3 +20,24 @@ std::string Date::monthNames[] = {
 		"MAY", "JUN", "JUL", "AUG",
 		"SEP", "OCT", "NOV", "DEC"
 };
+
+bool Date::_is_day_admissible(int day, int month, int year) {
+	if (_is_month_admissible(month)) {
+		// 30-napos honapok
+		if (month == 4 || month == 6 || month == 9 || month == 11) {
+			//if (day > 0 && day < 31) return true;
+			return (day > 0 && day < 31);
+		}
+		else if (month == 2) {
+			int finalDay = year % 4 == 0 ? 29 : 28;
+			return (day > 0 && day < finalDay + 1);
+		}
+		// ellenkezo esetben 31-napos a honap
+		return (day > 0 && day < 32);
+	}
+	return false;
+}
+
+bool Date::_is_month_admissible(int month) {
+	return (month < 13 && month > 0);
+}
--- ora-001-dates/date.h
+++ ora-001-dates/date.h
@@ -4,25 +4,8 @@
 class Date {
 	int _day, _month, _year;
 	static std::string monthNames[13];
-	static bool _is_day_admissible(int day, int month, int year) {
-		if (_is_month_admissible(month)) {
-			// 30-napos honapok
-			if (month == 4 || month == 6 || month == 9 || month == 11) {
-				//if (day > 0 && day < 31) return true;
-				return (day > 0 && day < 31);
-			}
-			else if (month == 2) {
-				int finalDay = year % 4 == 0 ? 29 : 28;
-				return (day > 0 && day < finalDay + 1);
-			}
-			// ellenkezo esetben 31-napos a honap
-			return (day > 0 && day < 32);
-		}
-		return false;
-	}
-	static bool _is_month_admissible(int month) {
-		return (month < 13 && month > 0);
-	}
+	static bool _is_day_admissible(int day, int month, int year);
+	static bool _is_month_admissible(int month);
 public:
 	Date(int day, int month, int year);
 	void print();
