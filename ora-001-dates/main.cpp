4d9090512df7cbb0d9ff38e478fa0c08dda0da06
Dr. Ádám Csapó <adam.csapo@maxwhere.com>
2022-09-09 10:44:52 +0200

    mi a hiba?

--- ora-001-dates/main.cpp
+++ ora-001-dates/main.cpp
@@ -1,6 +1,14 @@
 // main.cpp
 
-
+class Date {
+	int _day, _month, _year;
+	Date(int day, int month, int year) {
+		_day = day;
+		_month = month;
+		_year = year;
+	}
+	void print() {}
+};
 
 int main() {
 	Date jan1_1970(1, 1, 1970);
