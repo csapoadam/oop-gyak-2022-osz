5949ec3698fa824ce4bc1a352476efa4c281579d
Dr. Ádám Csapó <adam.csapo@maxwhere.com>
2022-09-09 10:57:45 +0200

    a honap ugy ahogy kiirva...

--- ora-001-dates/main.cpp
+++ ora-001-dates/main.cpp
@@ -1,4 +1,6 @@
 // main.cpp
+#include <string>
+#include <iostream>
 
 class Date {
 	int _day, _month, _year;
@@ -8,10 +10,18 @@ public:
 		_month = month;
 		_year = year;
 	}
-	void print() {}
+	void print() {
+		std::string monthNames[] = {
+			"JAN", "FEB", "MAR", "APR",
+			"MAY", "JUN", "JUL", "AUG",
+			"SEP", "OCT", "NOV", "DEC"
+		};
+		// pelda:
+		std::cout << monthNames[_month-1] << std::endl;
+	}
 };
 
 int main() {
 	Date jan1_1970(1, 1, 1970);
-	jan1_1970.print();
+	jan1_1970.print(); // Elvaras: JAN 1, 1970
 }
