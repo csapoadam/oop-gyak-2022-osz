436afd9dd80902c18975e8cc0add3295504ae419
Dr. Ádám Csapó <adam.csapo@maxwhere.com>
2022-09-09 11:59:36 +0200

    refactor: date osztaly kiszervezese - 1

---
+++ ora-001-dates/date.h
@@ -0,0 +1,47 @@
+#pragma once
+
+#include <string>
+#include <iostream>
+
+class Date {
+	int _day, _month, _year;
+	static std::string monthNames[13];
+	static bool _is_day_admissible(int day, int month, int year) {
+		if (_is_month_admissible(month)) {
+			// 30-napos honapok
+			if (month == 4 || month == 6 || month == 9 || month == 11) {
+				//if (day > 0 && day < 31) return true;
+				return (day > 0 && day < 31);
+			}
+			else if (month == 2) {
+				int finalDay = year % 4 == 0 ? 29 : 28;
+				return (day > 0 && day < finalDay + 1);
+			}
+			// ellenkezo esetben 31-napos a honap
+			return (day > 0 && day < 32);
+		}
+		return false;
+	}
+	static bool _is_month_admissible(int month) {
+		return (month < 13 && month > 0);
+	}
+public:
+	Date(int day, int month, int year) {
+		_month = _is_month_admissible(month) ? month : 0;
+		_day = _is_day_admissible(day, month, year) ? day : 0;
+		_year = year;
+	}
+	void print() {
+		std::cout <<
+			monthNames[_month] << " " <<
+			_day << ", " <<
+			_year << std::endl;
+	}
+};
+
+std::string Date::monthNames[] = {
+		"BADMONTH",
+		"JAN", "FEB", "MAR", "APR",
+		"MAY", "JUN", "JUL", "AUG",
+		"SEP", "OCT", "NOV", "DEC"
+};
