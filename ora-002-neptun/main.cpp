59f47b23a893318c5fc2afaa1388325f466e5967
Dr. Ádám Csapó <csapo.adam@gmail.com>
2022-09-16 11:00:22 +0200

    feladat: magasabb szinten hallgatoi db, ez kezelje a Hallgato objektumokat

--- ora-002-neptun/main.cpp
+++ ora-002-neptun/main.cpp
@@ -1,9 +1,10 @@
-#include "hallgato.h"
+#include "hallgatoDb.h"
 
 using namespace Neptun; // nagy projekteknel nem javasolt
 // mivel baj, ha 2 nevterben is letezik u.az a tipus vagy fv
 
 int main() {
-	Hallgato kocsisb("Kocsis Balint", "RBV23Q");
-	kocsisb.print(); // Kocsis Balint - RBV23Q
+	HallgatoDb hallgatok;
+	hallgatok.add("Kocsis Balint", "RBV23Q");
+	hallgatok.printDb();
 }
