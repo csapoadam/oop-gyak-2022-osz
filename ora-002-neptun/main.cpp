cd4893aedb0645053b79b62967878cfb6c80ab0f
Dr. Ádám Csapó <csapo.adam@gmail.com>
2022-09-16 10:40:13 +0200

    impl (naiv): Hallgato

--- ora-002-neptun/main.cpp
+++ ora-002-neptun/main.cpp
@@ -1,6 +1,18 @@
+#include <string>
+#include <iostream>
 
-
-
+class Hallgato {
+	std::string nev; // adatrejtes elve: adattagok jellemzoen privatak
+	std::string neptunkod;
+public:
+	Hallgato(std::string neve, std::string neptunkodja) {
+		nev = neve;
+		neptunkod = neptunkodja;
+	}
+	void print() {
+		std::cout << nev << " - " << neptunkod << std::endl;
+	}
+};
 
 int main() {
 	Hallgato kocsisb("Kocsis Balint", "RBV23Q");
