5a66e47f9d3da0c63ff04e9900d6e255f70e16c0
Dr. Ádám Csapó <csapo.adam@gmail.com>
2022-09-16 11:25:24 +0200

    mutatom: forEach iteralas kenyelmesebb

--- ora-002-neptun/hallgatoDb.cpp
+++ ora-002-neptun/hallgatoDb.cpp
@@ -10,7 +10,10 @@ void HallgatoDb::add(std::string nev, std::string neptunkod) {
 
 void HallgatoDb::printDb() {
 	std::cout << "Az egyetem hallgatoi:" << std::endl;
-	for (int inx = 0; inx < hallgatok.size(); inx++) {
+	/*for (int inx = 0; inx < hallgatok.size(); inx++) {
 		hallgatok[inx].print();
+	}*/
+	for (Hallgato h : hallgatok) { // igazabol referencia is lehetne, ld. majd kesobb
+		h.print();
 	}
 }
