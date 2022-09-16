9fdf1394f00c507b714f68930768683f762de5a9
Dr. Ádám Csapó <csapo.adam@gmail.com>
2022-09-16 11:21:43 +0200

    impl (naiv): HallgatoDb

---
+++ ora-002-neptun/hallgatoDb.cpp
@@ -0,0 +1,16 @@
+#include <iostream>
+#include "hallgatoDb.h"
+
+using namespace Neptun;
+
+void HallgatoDb::add(std::string nev, std::string neptunkod) {
+	// vectorba valo beszuras push_back() metodussal
+	hallgatok.push_back(Hallgato(nev, neptunkod));
+}
+
+void HallgatoDb::printDb() {
+	std::cout << "Az egyetem hallgatoi:" << std::endl;
+	for (int inx = 0; inx < hallgatok.size(); inx++) {
+		hallgatok[inx].print();
+	}
+}
--- ora-002-neptun/hallgatoDb.h
+++ ora-002-neptun/hallgatoDb.h
@@ -4,8 +4,10 @@
 
 namespace Neptun {
 	class HallgatoDb {
+		// std::map talan jobb lenne, de az std::vector alapveto ismeret
 		std::vector<Hallgato> hallgatok;
 	public:
+		HallgatoDb() {} // jelenleg nem szukseges, de sose baj
 		void add(std::string nev, std::string neptunkod);
 		void printDb();
 	};
