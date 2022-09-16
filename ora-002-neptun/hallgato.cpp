43e45d052c130caa680d94d4c5aa6f9aa0643d2a
Dr. Ádám Csapó <csapo.adam@gmail.com>
2022-09-16 11:12:48 +0200

    refactor: iostream-et eleg a cpp fajlbol include-olni

--- ora-002-neptun/hallgato.cpp
+++ ora-002-neptun/hallgato.cpp
@@ -1,4 +1,8 @@
 #include "hallgato.h"
+#include <iostream>
+
+// lehetne using namespace Neptun; is, es akkor nem kene
+// mindenhova odairni
 
 Neptun::Hallgato::Hallgato(std::string nv, std::string nkod) {
  nev = nv;
--- ora-002-neptun/hallgato.h
+++ ora-002-neptun/hallgato.h
@@ -1,7 +1,6 @@
 #pragma once
 
 #include <string>
-#include <iostream>
 
 namespace Neptun {
 
