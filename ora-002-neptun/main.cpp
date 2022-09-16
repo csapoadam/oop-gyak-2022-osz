3dfca020ff44e4a803f1613d509d05e3c7acd388
Dr. Ádám Csapó <csapo.adam@gmail.com>
2022-09-16 12:07:02 +0200

    feladat: targyak adatbazisa, targyfelvetel menedzselese

--- ora-002-neptun/main.cpp
+++ ora-002-neptun/main.cpp
@@ -12,4 +12,23 @@ int main() {
 	// feladat: mivel a neptun kod mar letezik, ne adjuk hozza:
 	hallgatok.add("Hollan Eva", "AABIK5"); // u.az a neptun kod mint kettovel feljebb!
 	hallgatok.printDb();
+
+	TargyDb targyak;
+	targyak.add("OO Programozas", "NGB1", 6);
+	targyak.add("Szoftverteszteles", "NGB5", 5);
+	// ne engedjuk azonos koddal
+	targyak.add("Adatbazisok", "NGB5", 2);
+	targyak.printDb();
+
+	// Melyik Hallgato mely Targyakat vette fel?
+	// ez a kerdes sem a HallgatoDb-hez, sem a TargyDb-hez
+	// nem kapcsolodik kizarolagosan. Ezert legyen egy
+	// ezt a kapcsolatot menedzselo osztaly:
+	FelvettTargyDb felvettTargyak;
+	felvettTargyak.add("AX873U", "NGB1"); // hallgato, targy
+	felvettTargyak.add("RBV23Q", "NGB1"); // hallgato, targy
+	felvettTargyak.add("AABIK5", "NGB5"); // hallgato, targy
+	felvettTargyak.printTargyakByHallgato("AX873U");
+	felvettTargyak.printHallgatokByTargy("NGB1");
+
 }
