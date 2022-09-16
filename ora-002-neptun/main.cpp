b4f7761e4fc74f99957e82b8ced59d1f5dd1170f
Dr. Ádám Csapó <csapo.adam@gmail.com>
2022-09-16 11:35:24 +0200

    feladat: azonos neptun kodok kiszurese

--- ora-002-neptun/main.cpp
+++ ora-002-neptun/main.cpp
@@ -8,5 +8,8 @@ int main() {
 	hallgatok.add("Kocsis Balint", "RBV23Q");
 	hallgatok.add("Szabo Tibor", "QKX7R1");
 	hallgatok.add("Nemeth Iren", "AABIK5");
+	hallgatok.add("Szabo Tibor", "AX873U"); // hiaba azonos nev, de egyedi neptun kod!
+	// feladat: mivel a neptun kod mar letezik, ne adjuk hozza:
+	hallgatok.add("Hollan Eva", "AABIK5"); // u.az a neptun kod mint kettovel feljebb!
 	hallgatok.printDb();
 }
