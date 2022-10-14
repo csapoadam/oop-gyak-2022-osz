3fabbf920e8773ddf591e83562fc64ff6668b5a4
Dr. Ádám Csapó <csapo.adam@gmail.com>
2022-10-14 12:02:00 +0200

    feladat: Egyseg nevu unio osztaly

--- ora-004-hadsereg/hadsereg.h
+++ ora-004-hadsereg/hadsereg.h
@@ -4,34 +4,38 @@
 #include <iostream>
 
 class Hadsereg {
-	std::vector<GyalogosEgyseg*> gyalogosok;
+	std::vector<Egyseg*> egysegek;
+	/*std::vector<GyalogosEgyseg*> gyalogosok;
 	std::vector<LegiEgyseg*> legiegysegek;
-	std::vector<HajosEgyseg*> hajosegysegek;
+	std::vector<HajosEgyseg*> hajosegysegek;*/
 public:
-	void addEgyseg(GyalogosEgyseg* ge) {
-		gyalogosok.push_back(ge);
+	void addEgyseg(Egyseg* ge) {
+		egysegek.push_back(ge);
 	}
-	void addEgyseg(LegiEgyseg* le) {
+	/*void addEgyseg(LegiEgyseg* le) {
 		legiegysegek.push_back(le);
 	}
 	void addEgyseg(HajosEgyseg* he) {
 		hajosegysegek.push_back(he);
-	}
+	}*/
 	void print() {
-		std::cout << "Gyalogos egysegek:" << std::endl;
-		for (GyalogosEgyseg* ge : gyalogosok) {
-			ge->print(1); // indent legyen 1
+		for (Egyseg* e : egysegek) {
+			e->print();
 		}
-		std::cout << "Legi egysegek:" << std::endl;
-		// foreach iteralas: auto is hasznalhato, hiszen
-		// a fordito ugyis tudja, hogy legiegysegek milyen tipusu
-		// dolgokat tarol
-		for (auto le : legiegysegek) {
-			le->print(1); // indent legyen 1
-		}
-		std::cout << "Hajos egysegek:" << std::endl;
-		for (auto he : hajosegysegek) {
-			he->print(1); // indent legyen 1
-		}
+		//std::cout << "Gyalogos egysegek:" << std::endl;
+		//for (GyalogosEgyseg* ge : gyalogosok) {
+		//	ge->print(1); // indent legyen 1
+		//}
+		//std::cout << "Legi egysegek:" << std::endl;
+		//// foreach iteralas: auto is hasznalhato, hiszen
+		//// a fordito ugyis tudja, hogy legiegysegek milyen tipusu
+		//// dolgokat tarol
+		//for (auto le : legiegysegek) {
+		//	le->print(1); // indent legyen 1
+		//}
+		//std::cout << "Hajos egysegek:" << std::endl;
+		//for (auto he : hajosegysegek) {
+		//	he->print(1); // indent legyen 1
+		//}
 	}
 };
--- ora-004-hadsereg/main.cpp
+++ ora-004-hadsereg/main.cpp
@@ -5,12 +5,16 @@
 
 int main() {
 	Hadsereg h1;
-	GyalogosEgyseg ge1(150, 47.49, 19.04); // 150 fo, GPS lat, GPS lon (Budapest)
-	LegiEgyseg le1(1500, 47.68, 17.63); // hany m magasan tud repulni, GPS lat, GPS lon, (Gyor)
-	// legenyseg szama: 300
-	// hatotavolsag: 15.5 km
-	// koordinatak (Sevastopol)
-	HajosEgyseg he1(300, 15.5, 44.62, 33.52);
+	// Egyseg tipusa:
+	// 0 - gyalogos
+	// 1 - legi
+	// 2 - hajos
+	// ez csunya megoldas de mukodhet ameddig az oroklesrol nem tanultunk!
+	// Feladat: Egyseg osztaly, ami mindent kezel
+	Egyseg ge1(0, 150, 47.49, 19.04); // 150 fo, GPS lat, GPS lon (Budapest)
+	Egyseg le1(1, 1500, 47.68, 17.63); // hany m magasan tud repulni, GPS lat, GPS lon, (Gyor)
+	// legenyseg szama: 300, hatotavolsag: 15.5 km, koordinatak (Sevastopol)
+	Egyseg he1(2, 300, 15.5, 44.62, 33.52);
 	h1.addEgyseg(&ge1);
 	h1.addEgyseg(&le1);
 	h1.addEgyseg(&he1);
