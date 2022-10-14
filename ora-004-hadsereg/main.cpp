46cd7368f43b271058648599e691db8efb29c57b
Dr. Ádám Csapó <csapo.adam@gmail.com>
2022-10-14 11:41:51 +0200

    feladat: hajos egyseg

--- ora-004-hadsereg/main.cpp
+++ ora-004-hadsereg/main.cpp
@@ -7,8 +7,13 @@ int main() {
 	Hadsereg h1;
 	GyalogosEgyseg ge1(150, 47.49, 19.04); // 150 fo, GPS lat, GPS lon (Budapest)
 	LegiEgyseg le1(1500, 47.68, 17.63); // hany m magasan tud repulni, GPS lat, GPS lon, (Gyor)
+	// legenyseg szama: 300
+	// hatotavolsag: 15.5 km
+	// koordinatak (Sevastopol)
+	HajosEgyseg he1(300, 15.5, 44.62, 33.52);
 	h1.addEgyseg(&ge1);
 	h1.addEgyseg(&le1);
+	h1.addEgyseg(&he1);
 
 	h1.print();
 }
