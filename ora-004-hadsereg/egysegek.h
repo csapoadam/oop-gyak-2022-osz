da9f30fa002c7d320a4f4c3112f3f3bfc01b0a48
Dr. Ádám Csapó <csapo.adam@gmail.com>
2022-10-14 11:15:23 +0200

    elso naiv impl

--- ora-004-hadsereg/egysegek.h
+++ ora-004-hadsereg/egysegek.h
@@ -1,13 +1,39 @@
 #pragma once
+#include <iostream>
 
+void printIndents(int indent) {
+	for (int ind = 0; ind < indent; ind++) {
+		std::cout << "\t";
+	}
+}
 
 class GyalogosEgyseg {
-
+	int hanyFo;
+	double gpsLatitude, gpsLongitude;
 public:
-	GyalogosEgyseg(int hanyFo, double gpslat, double gpslon) {}
+	GyalogosEgyseg(int hanyFo, double gpslat, double gpslon):
+		hanyFo(hanyFo), gpsLatitude(gpslat), gpsLongitude(gpslon) {}
+	void print(int indent) {
+		printIndents(indent);
+		std::cout << hanyFo << " fobol allo csapat" << std::endl;
+		printIndents(indent);
+		std::cout << "GPS koordinatak: " << gpsLatitude << ", ";
+		std::cout << gpsLongitude << std::endl;
+	}
 };
 
 class LegiEgyseg {
+	int maxMagassag;
+	double gpsLatitude, gpsLongitude;
 public:
-	LegiEgyseg(int maxmagassag, double gpslat, double gpslon) {}
+	LegiEgyseg(int maxmagassag, double gpslat, double gpslon):
+		maxMagassag(maxmagassag), gpsLatitude(gpslat), gpsLongitude(gpslon)
+	{}
+	void print(int indent) {
+		printIndents(indent);
+		std::cout << "maximum magassag: " << maxMagassag << std::endl;
+		printIndents(indent);
+		std::cout << "GPS koordinatak: " << gpsLatitude << ", ";
+		std::cout << gpsLongitude << std::endl;
+	}
 };
--- ora-004-hadsereg/hadsereg.h
+++ ora-004-hadsereg/hadsereg.h
@@ -1,10 +1,29 @@
 #pragma once
 #include "egysegek.h"
+#include <vector>
+#include <iostream>
 
 class Hadsereg {
-
+	std::vector<GyalogosEgyseg*> gyalogosok;
+	std::vector<LegiEgyseg*> legiegysegek;
 public:
-	void addEgyseg(GyalogosEgyseg* ge) {}
-	void addEgyseg(LegiEgyseg*) {}
-	void print() {}
+	void addEgyseg(GyalogosEgyseg* ge) {
+		gyalogosok.push_back(ge);
+	}
+	void addEgyseg(LegiEgyseg* le) {
+		legiegysegek.push_back(le);
+	}
+	void print() {
+		std::cout << "Gyalogos egysegek:" << std::endl;
+		for (GyalogosEgyseg* ge : gyalogosok) {
+			ge->print(1); // indent legyen 1
+		}
+		std::cout << "Legi egysegek:" << std::endl;
+		// foreach iteralas: auto is hasznalhato, hiszen
+		// a fordito ugyis tudja, hogy legiegysegek milyen tipusu
+		// dolgokat tarol
+		for (auto le : legiegysegek) {
+			le->print(1); // indent legyen 1
+		}
+	}
 };
