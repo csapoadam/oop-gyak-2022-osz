09a999cc38055467cdca823151c87c8a458906f0
Dr. Ádám Csapó <csapo.adam@gmail.com>
2022-10-14 11:25:22 +0200

    feladat: gps refaktoralas mert lat es lon kulon-kulon nem rendelkezik letjogosultsaggal!

--- ora-004-hadsereg/egysegek.h
+++ ora-004-hadsereg/egysegek.h
@@ -7,33 +7,35 @@ void printIndents(int indent) {
 	}
 }
 
+class GPSKoordinata {
+
+};
+
 class GyalogosEgyseg {
 	int hanyFo;
-	double gpsLatitude, gpsLongitude;
+	GPSKoordinata gps;
 public:
 	GyalogosEgyseg(int hanyFo, double gpslat, double gpslon):
-		hanyFo(hanyFo), gpsLatitude(gpslat), gpsLongitude(gpslon) {}
+		hanyFo(hanyFo), gps(gpslat, gpslon) {}
 	void print(int indent) {
 		printIndents(indent);
 		std::cout << hanyFo << " fobol allo csapat" << std::endl;
 		printIndents(indent);
-		std::cout << "GPS koordinatak: " << gpsLatitude << ", ";
-		std::cout << gpsLongitude << std::endl;
+		gps.print();
 	}
 };
 
 class LegiEgyseg {
 	int maxMagassag;
-	double gpsLatitude, gpsLongitude;
+	GPSKoordinata gps;
 public:
 	LegiEgyseg(int maxmagassag, double gpslat, double gpslon):
-		maxMagassag(maxmagassag), gpsLatitude(gpslat), gpsLongitude(gpslon)
+		maxMagassag(maxmagassag), gps(gpslat, gpslon)
 	{}
 	void print(int indent) {
 		printIndents(indent);
 		std::cout << "maximum magassag: " << maxMagassag << std::endl;
 		printIndents(indent);
-		std::cout << "GPS koordinatak: " << gpsLatitude << ", ";
-		std::cout << gpsLongitude << std::endl;
+		gps.print();
 	}
 };
