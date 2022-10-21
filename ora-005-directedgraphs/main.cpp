f89d2f50bc84bae9264913ca850e0f8d3016acf1
Dr. Ádám Csapó <csapo.adam@gmail.com>
2022-10-21 11:18:37 +0200

    koztes feladat

--- ora-005-directedgraphs/main.cpp
+++ ora-005-directedgraphs/main.cpp
@@ -6,16 +6,30 @@ int main() {
 	graph.addChild("root", ".A"); // root-hoz hozzadjuk a .A nevu csomopontot
 	graph.addChild("root", ".B");
 
+	graph.addChild(".B", ".BA");
+
 	graph.addChild(".A", ".AA");
 	graph.addChild(".A", ".AB");
 
-	graph.addChild(".B", ".BA");
 	graph.addChild(".B", ".BB");
 	graph.addChild(".B", ".BC");
-	
-	graph.printBFS(); //szelessegi kereses: Breadth-First Search
-	// "root", ".A", ".B", ".AA", ".AB", ".BA", ".BB", ".BC"
 
-	graph.printDFS(); // melysegi kereses: Depth-First Search
-	// "root", ".A", ".AA", ".AB", ".B", ".BA", ".BB", ".BC"
+	graph.printNodes(); // hozzaadas sorrendjeben menjen...
+	//// "root", ".A", ".B", ".BA", ".AA", ".AB", ".BB", ".BC"
+
+	graph.printNodesWithChildren; // hozzaadas sorrendjeben gyerekekkel egyutt
+	//// "root", ".A", ".B"
+	//// ".A", ".AA", ".AB"
+	//// ".B", ".BA", ".BB", ".BC"
+	//// ".BA"
+	//// ".AA"
+	//// ".AB"
+	//// ".BB"
+	//// ".BC"
+	
+	//graph.printBFS(); //szelessegi kereses: Breadth-First Search
+	//// "root", ".A", ".B", ".AA", ".AB", ".BA", ".BB", ".BC"
+
+	//graph.printDFS(); // melysegi kereses: Depth-First Search
+	//// "root", ".A", ".AA", ".AB", ".B", ".BA", ".BB", ".BC"
 }
