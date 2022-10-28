4fb4cdc964387c88d150a906ba77166b215f6803
Dr. Ádám Csapó <csapo.adam@gmail.com>
2022-10-28 12:23:52 +0200

    feladat: refactor

--- ora-006b-hadsereg-oroklessel/main.cpp
+++ ora-006b-hadsereg-oroklessel/main.cpp
@@ -10,11 +10,13 @@ int main() {
 	// 1 - legi
 	// 2 - hajos
 	// ez csunya megoldas de mukodhet ameddig az oroklesrol nem tanultunk!
-	// Feladat: Egyseg osztaly, ami mindent kezel
-	Egyseg ge1(0, 150, 47.49, 19.04); // 150 fo, GPS lat, GPS lon (Budapest)
-	Egyseg le1(1, 1500, 47.68, 17.63); // hany m magasan tud repulni, GPS lat, GPS lon, (Gyor)
+	// Feladat: Irjuk at a korabbi megoldast, ugy, hogy ne kelljen
+	// ilyen csunya tipusmezoket hasznalni!
+	
+	GyalogosEgyseg ge1(150, 47.49, 19.04); // 150 fo, GPS lat, GPS lon (Budapest)
+	LegiEgyseg le1(1500, 47.68, 17.63); // hany m magasan tud repulni, GPS lat, GPS lon, (Gyor)
 	// legenyseg szama: 300, hatotavolsag: 15.5 km, koordinatak (Sevastopol)
-	Egyseg he1(2, 300, 15.5, 44.62, 33.52);
+	HajosEgyseg he1(300, 15.5, 44.62, 33.52);
 	h1.addEgyseg(&ge1);
 	h1.addEgyseg(&le1);
 	h1.addEgyseg(&he1);
