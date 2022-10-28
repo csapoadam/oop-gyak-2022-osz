cab7ab8f164792d1e5d81facca00d93c1eeccc14
Dr. Ádám Csapó <csapo.adam@gmail.com>
2022-10-28 10:29:56 +0200

    orokles: kiindulo feladat

---
+++ ora-006-orokles/main.cpp
@@ -0,0 +1,17 @@
+#include <vector>
+
+
+int main() {
+	Laptop l1("Asus", "i7", 16);
+	Okostelefon tel1("Samsung", "S22");
+	Okosora ora1("Garmin", "Vivoactive 4");
+
+	std::vector<Eszkoz*> eszkozok;
+	eszkozok.push_back(&l1);
+	eszkozok.push_back(&tel1);
+	eszkozok.push_back(&ora1);
+
+	for (Eszkoz* e : eszkozok) {
+		e->print();
+	}
+}
