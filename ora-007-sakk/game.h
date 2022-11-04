889298d6b509c4324a3c96dded27f0e3156d09bb
Dr. Ádám Csapó <csapo.adam@gmail.com>
2022-11-04 12:42:50 +0100

    feladat: draw() metodus javitasa, ChessFigure es Pawn osztalyok megirasa

--- ora-007-sakk/game.h
+++ ora-007-sakk/game.h
@@ -1,11 +1,59 @@
 #pragma once
 #include <iostream>
 #include <string>
+#include <vector>
 
 class ChessGame {
 	bool isWhiteTurn;
+	std::vector < std::vector<ChessFigure*> > tableRows;
 public:
-	ChessGame() : isWhiteTurn(true) {}
+	ChessGame() : isWhiteTurn(true) {
+		std::vector<ChessFigure*> row1;
+		tableRows.push_back(row1);
+		for (int i = 0; i < 8; i++) {
+			// ide majd mas kell, most legyenek gyalogok
+			row1.push_back(new Pawn("white"));
+		}
+
+		std::vector<ChessFigure*> row2;
+		for (int i = 0; i < 8; i++) {
+			row2.push_back(new Pawn("white"));
+		}
+
+		tableRows.push_back(row2);
+		std::vector<ChessFigure*> row3;
+		for (int i = 0; i < 8; i++) {
+			row3.push_back(nullptr);
+		}
+		tableRows.push_back(row3);
+		std::vector<ChessFigure*> row4;
+		for (int i = 0; i < 8; i++) {
+			row4.push_back(nullptr);
+		}
+		tableRows.push_back(row4);
+		std::vector<ChessFigure*> row5;
+		tableRows.push_back(row5);
+		for (int i = 0; i < 8; i++) {
+			row5.push_back(nullptr);
+		}
+		std::vector<ChessFigure*> row6;
+		tableRows.push_back(row6);
+		for (int i = 0; i < 8; i++) {
+			row3.push_back(nullptr);
+		}
+		std::vector<ChessFigure*> row7;
+		for (int i = 0; i < 8; i++) {
+			row7.push_back(new Pawn("black"));
+		}
+		tableRows.push_back(row7);
+
+		std::vector<ChessFigure*> row8;
+		for (int i = 0; i < 8; i++) {
+			// ide majd mas kell, most legyenek gyalogok
+			row1.push_back(new Pawn("black"));
+		}
+		tableRows.push_back(row8);
+	}
 	void draw() {
 		std::cout << "  | A | B | C | D | E | F | G | H |";
 		std::cout << std::endl;
