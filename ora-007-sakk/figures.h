19f32d43f1dabb14548cc130cbf328b0a1a610b5
Dr. Ádám Csapó <csapo.adam@gmail.com>
2022-11-11 11:47:03 +0100

    tisztek impl. Feladat: refactor!

--- ora-007-sakk/figures.h
+++ ora-007-sakk/figures.h
@@ -11,14 +11,54 @@ public:
 
 class Pawn : public ChessFigure {
 public:
-	Pawn(const std::string& color) :
-		ChessFigure(color) {}
+	Pawn(const std::string& color) : ChessFigure(color) {}
 	std::string asString() {
-		if (getColor() == "white") {
-			return "wP";
-		}
-		else {
-			return "bP";
-		}
+		if (getColor() == "white") return "wP";
+		return "bP";
+	}
+};
+
+class Rook : public ChessFigure {
+public:
+	Rook(const std::string& color) : ChessFigure(color) {}
+	std::string asString() {
+		if (getColor() == "white") return "wR";
+		return "bR";
+	}
+};
+
+class Knight : public ChessFigure {
+public:
+	Knight(const std::string& color) : ChessFigure(color) {}
+	std::string asString() {
+		if (getColor() == "white") return "wN";
+		return "bN";
+	}
+};
+
+class Bishop : public ChessFigure {
+public:
+	Bishop(const std::string& color) : ChessFigure(color) {}
+	std::string asString() {
+		if (getColor() == "white") return "wB";
+		return "bB";
+	}
+};
+
+class Queen : public ChessFigure {
+public:
+	Queen(const std::string& color) : ChessFigure(color) {}
+	std::string asString() {
+		if (getColor() == "white") return "wQ";
+		return "bQ";
+	}
+};
+
+class King : public ChessFigure {
+public:
+	King(const std::string& color) : ChessFigure(color) {}
+	std::string asString() {
+		if (getColor() == "white") return "wK";
+		return "bK";
 	}
 };
