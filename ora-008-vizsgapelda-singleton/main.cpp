3875a9750d3473bf4e5ab5589f6eefbe24fb14e0
Dr. Ádám Csapó <csapo.adam@gmail.com>
2022-11-18 11:33:10 +0100

    abstract es singleton osztaly megvalositasa

--- ora-008-vizsgapelda-singleton/main.cpp
+++ ora-008-vizsgapelda-singleton/main.cpp
@@ -1,4 +1,5 @@
 #include <iostream>
+#include <string>
 #include <type_traits>
 
 // A 2-es jegyert keszitse el az alabbi osztalyokat:
@@ -7,6 +8,14 @@
 // - ennek az osztalynak van egy const std::string&-et varo konstruktora, ami beallitja az Oltas nevet!
 // - ennek az osztalynak van egy getName() metodusa ami visszaadja az oltas nevet
 // - ennek az osztalynak van egy getType() nevu pure virtual metodusa is, ami std::string-et ad vissza, de nincs kifejtve!
+
+class Oltas {
+    std::string name;
+public:
+    Oltas(const std::string& name) : name(name) {}
+    std::string getName() { return name; }
+    virtual std::string getType() = 0;
+};
 
 // Pfizer osztaly, amely publikus modon szarmazik az Oltas osztalybol
 // Ez egy singleton osztaly, vagyis:
@@ -17,13 +26,29 @@
 // - megvalositja az ososztaly getType() metodusat is, es azt a stringet adja vissza, hogy "mRNA"
 // Singleton osztalyokrol hatteranyag: https://u.pcloud.link/publink/show?code=XZMR4rXZsHduf1qXGq4bAzr2cNPaCX5friLV (8-9. oldal)
 
+class Pfizer : public Oltas {
+    Pfizer(const std::string& name) : Oltas(name) {}
+    Pfizer(Pfizer& other) = delete;
+    Pfizer& operator=(Pfizer& other) = delete;
+public:
+    static Oltas* GetInstance() {
+        static Pfizer instance("Pfizer");
+        return &instance;
+    }
+    std::string getType() {
+        return "mRNA";
+    }
+};
+
 
 // elvart kimenet:
 //Igen, pfizer es pfizer2 cime ugyanaz!
 //Pfizer neve es tipusa : Pfizer, mRNA
 
+
 int main() {
     static_assert(std::is_abstract<Oltas>(), "Oltas-nak absztrakt osztalynak kell lennie!");
+
     Oltas* pfizer = Pfizer::GetInstance();
     Oltas* pfizer2 = Pfizer::GetInstance();
 
